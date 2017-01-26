999772076cf38d4ee0234822cc6ec0474221d84c
Toshiba <Toshiba@192.168.2.9>
2017-01-26 21:10:35 +0530

    C

--- CodingExamples/src/com/wintal/java8/codeExamples/Java8Features.java
+++ CodingExamples/src/com/wintal/java8/codeExamples/Java8Features.java
@@ -1,8 +1,9 @@
-package com.wintal.codeExamples;
+package com.wintal.java8.codeExamples;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Random;
 
 public class Java8Features {
 
@@ -10,9 +11,13 @@ public class Java8Features {
 		List<Integer> list = new ArrayList<Integer>();
 		list.add(5);list.add(4);list.add(3);list.add(2);list.add(1);
 		
+		list.forEach(System.out::println);
+		
 		Java8Features features = new Java8Features();
 		features.sortUsingJava7(list);
 		System.out.println(list);
+		
+		features.streamsExample();
 	}
 	
 	public void sortUsingJava7(List<Integer> list1){
@@ -25,6 +30,12 @@ public class Java8Features {
 			}
 		});
 		
+	}
+	
+	private void streamsExample(){
+		
+		Random random = new Random();
+		random.ints().limit(10).forEach(System.out::println);
 	}
 
 }
--- CodingExamples/src/com/wintal/java8/codeExamples/SendEmail.java
+++ CodingExamples/src/com/wintal/java8/codeExamples/SendEmail.java
@@ -1,3 +1,4 @@
+package com.wintal.java8.codeExamples;
 /*package com.wintal.codeExamples;
 
 //File Name SendEmail.java
