86fbe40691be2368f967ef9029981b69dba5ec74
Toshiba <Toshiba@192.168.2.9>
2017-01-28 02:22:31 +0530

    dateTime method

--- CodingExamples/src/com/wintal/java8/codeExamples/Java8Features.java
+++ CodingExamples/src/com/wintal/java8/codeExamples/Java8Features.java
@@ -1,9 +1,15 @@
 package com.wintal.java8.codeExamples;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Random;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Java8Features {
 
@@ -18,6 +24,7 @@ public class Java8Features {
 		System.out.println(list);
 		
 		features.streamsExample();
+		features.dateTimeJ8();
 	}
 	
 	public void sortUsingJava7(List<Integer> list1){
@@ -36,6 +43,30 @@ public class Java8Features {
 		
 		Random random = new Random();
 		random.ints().limit(10).forEach(System.out::println);
+		
+		List<Integer> list = new ArrayList<Integer>();
+		list.add(5);list.add(4);list.add(3);list.add(2);list.add(1);
+		
+		List<Integer> squaresList = list.stream().map(i -> i*i).distinct().collect(Collectors.toList());
+		
+		
+		squaresList.forEach(System.out::println);
 	}
+	
+	private void dateTimeJ8(){
+		
+		Set<String> zoneSet = ZoneId.getAvailableZoneIds();
+		LocalDateTime dateTime = LocalDateTime.now();
+		
+		for(String zone : zoneSet){
+			ZoneId id = ZoneId.of(zone);
+			ZonedDateTime time = dateTime.atZone(id);
+			ZoneOffset offset = time.getOffset();
+			System.out.println(zone+" "+offset);
+		}
+		
+	}
+	
+	
 
 }
